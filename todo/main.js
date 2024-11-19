f9c77487307c949c42e1c747cc5f65de39e8abd3
pjh <ioop2551@gmail.com>
2024-11-19 14:51:39 +0900

    feat: 로컬스토리지...

--- todo/main.js
+++ todo/main.js
@@ -3,15 +3,12 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
 const addBtn = document.querySelector('.form__btn');
 
-// TODO
-// 로컬호스트 저장
-// 정렬
-
 form.addEventListener('submit', (e) => {
 	addItem();
 	e.preventDefault();
 });
 
+// todo 추가하기
 function addItem() {
 	let text = input.value;
 	if (text.trim() === '') {
@@ -24,23 +21,58 @@ function addItem() {
 	input.focus();
 }
 
+// todo 만들기
 function createItem(text, id) {
 	const li = `
   <li class="item">
     <div>
-      <input type="checkbox" id="${id}"/>
-      <label for="${id}" class="item__label">${text}</label>
+      <input type="checkbox" id="id-${id}"/>
+      <label for="id-${id}" class="item__label">${text}</label>
     </div>
     <button class="item__btn"><i class="fa-solid fa-trash"></i></button>
   </li>
   `;
-
 	items.insertAdjacentHTML('beforeend', li);
+	setTodo(id, text);
 }
 
+// todo 삭제하기
 items.addEventListener('click', (e) => {
 	const btn = e.target.closest('.item__btn');
 	if (!btn) return;
 	const li = btn.closest('.item');
 	li.remove();
 });
+
+const todoData = [];
+
+// 불러오기
+function getTodo() {
+	let getData = localStorage.getItem('todolist');
+	getData = JSON.parse(getData);
+	if (!getData) return;
+	initTodo(getData);
+}
+getTodo();
+
+// 저장하기
+function setTodo(id, text) {
+	todoData.push({ id, text, isChecked: false });
+	console.log(todoData);
+	localStorage.setItem('todolist', JSON.stringify(todoData));
+}
+
+function initTodo(todolist) {
+	todolist.forEach(({ id, text, isChecked }) => {
+		const li = `
+      <li class="item">
+        <div>
+          <input type="checkbox" id="id-${id}" ${isChecked ? 'checked' : ''}/>
+          <label for="id-${id}" class="item__label">${text}</label>
+        </div>
+        <button class="item__btn"><i class="fa-solid fa-trash"></i></button>
+      </li>
+    `;
+		items.insertAdjacentHTML('beforeend', li);
+	});
+}
