c9ce997bf29e4bb507f31b9ffe240dbcab31e5d2
pjh <ioop2551@gmail.com>
2024-11-20 15:33:31 +0900

    todo: 전체삭제능

--- todo/main.js
+++ todo/main.js
@@ -2,8 +2,38 @@ const items = document.querySelector('.list');
 const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
 const addBtn = document.querySelector('.form__btn');
+const todos = document.querySelector('.list');
+const todo = document.querySelector('.delete-done');
+
+const btns = document.querySelector('.btns');
+const filterDone = document.querySelector('.filter-done');
+const filterUndone = document.querySelector('.filter-undone');
+const btnDelAll = document.querySelector('.delete-all');
+const btnDelDone = document.querySelector('.delete-done');
 
 let todoData = [];
+
+// 완료 목록
+
+// 미완료 목록
+
+// 전체 삭제
+btnDelAll.addEventListener('click', clearTodos);
+function clearTodos() {
+	if (todoData.length <= 0) {
+		// alert('삭제할 할일이 없습니다.');
+		return;
+	}
+	if (confirm('전체 삭제하시겠습니까?')) {
+		todoData = [];
+		localStorage.removeItem('todolist');
+		todos.innerHTML = '';
+	}
+}
+// 완료 삭제
+btnDelDone.addEventListener('click', () => {
+	// todoData
+});
 
 form.addEventListener('submit', (e) => {
 	addItem();
@@ -14,15 +44,16 @@ items.addEventListener('click', (e) => {
 	const btn = e.target.closest('.item__btn');
 	if (!btn) return;
 	const li = btn.closest('.item');
+	const div = btn.closest('.todo-item');
 	const id = li.querySelector('input').id.replace('id-', '');
-	console.log(id);
-
-	todoData = todoData.filter((todo) => todo.id !== id);
-	saveTodo(todoData);
-
-	li.remove();
+	removeItem();
 });
 
+function removeItem() {
+	todoData = todoData.filter((todo) => todo.id !== id);
+	saveTodo(todoData);
+	li.remove();
+}
 function addItem() {
 	let text = input.value;
 	if (text.trim() === '') {
@@ -34,6 +65,9 @@ function addItem() {
 	todoData.push(newTodo);
 	addTodo(newTodo);
 	saveTodo(todoData);
+
+	const addedItem = items.lastElementChild;
+	addedItem.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
 	input.value = '';
 	input.focus();
@@ -48,7 +82,7 @@ function createElement(id, text, isChecked) {
 	return `
   <li class="item">
     <div>
-      <input type="checkbox" id="id-${id}"/>
+      <input type="checkbox" id="id-${id}" ${isChecked ? 'checked' : ''}/>
       <label for="id-${id}" class="item__label">${text}</label>
     </div>
     <button class="item__btn"><i class="fa-solid fa-trash"></i></button>
